e3590f71ad231b474b352e7a5fa91b065fa572bd
AkhmetovSR <ahmetov26@gmail.com>
2024-10-16 00:41:23 +0500

    //new2

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,11 +1,13 @@
 import s from  "./Main.module.css";
 import searchIcon from "../resource/searchIcon.webp";
+import Sun from "../resource/sun.webp";
 import Writer from "./Writer/Writer.jsx";
 import Menu from "./Menu/Menu.jsx";
 
 function Main() {
   return (
     <div className={s.Main}>
+        <div className={s.Sun}><img src={Sun}/></div>
         <div className={s.Search}>
             <div className={s.SearchText}><Writer/></div>
             <div className={s.SearchIcon}><img className={s.searchImg} src={searchIcon}/></div>
