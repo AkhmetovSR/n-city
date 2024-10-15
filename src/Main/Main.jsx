7b0785b6bacab61931ba0c260a0bd25597e29c90
AkhmetovSR <ahmetov26@gmail.com>
2024-10-15 23:56:58 +0500

    //new1

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,9 +1,16 @@
 import s from  "./Main.module.css";
+import searchIcon from "../resource/searchIcon.webp";
+import Writer from "./Writer/Writer.jsx";
+import Menu from "./Menu/Menu.jsx";
 
 function Main() {
   return (
     <div className={s.Main}>
-      <input className={s.Search}/>
+        <div className={s.Search}>
+            <div className={s.SearchText}><Writer/></div>
+            <div className={s.SearchIcon}><img className={s.searchImg} src={searchIcon}/></div>
+        </div>
+        <Menu/>
     </div>
   );
 }
---
+++ src/Main/Menu/Menu.jsx
@@ -0,0 +1,31 @@
+import s from "./Menu.module.css";
+
+function Menu() {
+    return (
+        <div className={s.Menu}>
+            <div className={s.Options}>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default Menu;
