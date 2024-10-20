5484b32192145c9ddc9594b1d67092b693f7f7ca
shiis <ahmetov26@gmail.com>
2024-10-21 01:22:37 +0500

    //shiska1

--- src/Main/Widget/Widget.jsx
+++ src/Main/Widget/Widget.jsx
@@ -1,5 +1,5 @@
 import s from "./Widget.module.css";
-import w1 from "../../resource/Group 21.png";
+import w1 from "../../resource/Mask.webp";
 import w2 from "../../resource/Group 22.png";
 import w3 from "../../resource/Group 23.png";
 import w4 from "../../resource/Group 24.png";
@@ -9,7 +9,7 @@ import w6 from "../../resource/Group 26.png";
 
 function Widget() {
     const widgets = [
-        {id: 1, text: "widget1", img: w1},
+        {id: 1, text: "Афиша", img: w1},
         {id: 2, text: "widget2", img: w2},
         {id: 3, text: "widget3", img: w3},
         {id: 4, text: "widget4", img: w4},
@@ -18,7 +18,10 @@ function Widget() {
     ]
     const wdg = widgets.map(widget =>
         <div key={widget.id} className={s.Widgets}>
-            <div className={s.Widget}><img className={s.WdgImage} src={widget.img} alt="widget"/></div>
+            <div className={s.Widget}>
+                <img className={s.WdgImage} src={widget.img} alt="widget"/>
+                <div className={s.Footer}>{widget.text}</div>
+            </div>
         </div>
     )
     return (
